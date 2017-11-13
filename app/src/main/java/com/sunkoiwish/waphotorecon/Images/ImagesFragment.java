f85fc6a772c0c159a8d40925c0070428b15ae837
xaiyeon <hidevroyce@gmail.com>
2017-11-12 21:53:26 -0800

    Did some work for Microsoft cog serv in image frag Need to figure out JSON
    Merge remote-tracking branch 'github/master'
    
    Signed-off-by: xaiyeon <hidevroyce@gmail.com>
    
    # Conflicts:
    #	.idea/vcs.xml

--- app/src/main/java/com/sunkoiwish/waphotorecon/Images/ImagesFragment.java
+++ app/src/main/java/com/sunkoiwish/waphotorecon/Images/ImagesFragment.java
@@ -1,5 +1,6 @@
 package com.sunkoiwish.waphotorecon.Images;
 
+import android.app.ProgressDialog;
 import android.content.Context;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
@@ -7,6 +8,7 @@ import android.net.Uri;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -14,9 +16,19 @@ import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.google.gson.Gson;
 import com.microsoft.projectoxford.vision.VisionServiceClient;
 import com.microsoft.projectoxford.vision.VisionServiceRestClient;
+import com.microsoft.projectoxford.vision.contract.AnalysisResult;
+import com.microsoft.projectoxford.vision.contract.Caption;
+import com.microsoft.projectoxford.vision.contract.Face;
 import com.sunkoiwish.waphotorecon.R;
+
+import org.w3c.dom.Text;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.InputStream;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -27,6 +39,9 @@ import com.sunkoiwish.waphotorecon.R;
  * create an instance of this fragment.
  */
 public class ImagesFragment extends Fragment {
+
+    private String TAG = "ImagesFragment";
+
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
@@ -48,12 +63,14 @@ public class ImagesFragment extends Fragment {
      */
 
     // My subscription key for microsoft cog service
-    public VisionServiceClient visionServiceClient = new VisionServiceRestClient("c8fc6c87dde64e5888e49d65cf3d2569");
+
+    VisionServiceRestClient visionServiceRestClient = new VisionServiceRestClient("c8fc6c87dde64e5888e49d65cf3d2569", "https://westcentralus.api.cognitive.microsoft.com/vision/v1.0");
 
     // view widgets
     ImageView imageView;
     Button mbutton;
     TextView desc_txtView;
+    TextView age_txtView;
 
     public ImagesFragment() {
         // Required empty public constructor
@@ -91,7 +108,7 @@ public class ImagesFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        View view = inflater.inflate(R.layout.fragment_images, container, false);
+        final View view = inflater.inflate(R.layout.fragment_images, container, false);
 
         Bitmap bitmap = BitmapFactory.decodeResource(getResources(), R.drawable.sad_test);
         // fINDING OUR view objects
@@ -99,17 +116,99 @@ public class ImagesFragment extends Fragment {
         imageView = (ImageView) view.findViewById(R.id.fragimg_imageView);
         mbutton = (Button) view.findViewById(R.id.fragimg_submitbtn);
         desc_txtView = (TextView) view.findViewById(R.id.fragimg_desctxtview);
+        age_txtView = (TextView) view.findViewById(R.id.fragimg_faceage_txtview);
 
         imageView.setImageBitmap(bitmap);
+
+        // Convert the image to stream
+        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+        bitmap.compress(Bitmap.CompressFormat.JPEG, 100, outputStream);
+        final ByteArrayInputStream inputStream = new ByteArrayInputStream(outputStream.toByteArray());
 
         // our button click
         mbutton.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v) {
+            public void onClick(final View v) {
 
-                // TODO: Left off here for tutorial on Microsfot Cognitive Service Test
+                Log.d(TAG, "buttonClickMicrosoft: Button pressed.");
+                // Here we have an async tasks that will return our results from Microsoft
+                final AsyncTask<InputStream, String, String> visionTask = new AsyncTask<InputStream, String, String>() {
 
-               // AsyncTask<>
+                    ProgressDialog prgDialog = new ProgressDialog(getContext());
+                    @Override
+                    protected String doInBackground(InputStream... params) {
+                        try {
+                            publishProgress("Recognizing...");
+                            // we add what features we want.
+                            // TODO: Implement the other information from the json file
+                            String[] features = {"Description","Tags","ImageType", "Color", "Faces", "Adult", "Categories"};
+                            String[] details = {};
+                            AnalysisResult result = visionServiceRestClient.analyzeImage(params[0], features, details);
+
+                            // This stores the gotten data into a string that is of Gson
+                            String strResult = new Gson().toJson(result);
+                            return strResult;
+
+                        } catch (Exception e){
+                            return null;
+                        }
+                    }
+
+                    // genereated, alt + inst Override
+
+                    @Override
+                    protected void onPreExecute() {
+                        prgDialog.show();
+                    }
+
+                    @Override
+                    protected void onPostExecute(String s) {
+                        prgDialog.dismiss();
+
+                        // Convert back the string to object.
+                        AnalysisResult result = new Gson().fromJson(s, AnalysisResult.class);
+                        // had to declare as final View
+                        desc_txtView = (TextView) view.findViewById(R.id.fragimg_desctxtview);
+                        age_txtView = (TextView) view.findViewById(R.id.fragimg_faceage_txtview);
+
+                        StringBuilder desc_SB = new StringBuilder();
+                        StringBuilder faceage_SB = new StringBuilder();
+
+                        // TODO: Confused on above and here...
+                        //for(Face face:result.faces.)
+
+//                        StringBuilder stringBuilder = new StringBuilder();
+//
+//                        // Added this for faces stringBuilder
+//                        StringBuilder facestringBuilder = new StringBuilder();
+//                        // String Face;
+//
+//                        // age
+//                        for(Face face:result.faces){
+//                            facestringBuilder.append(face.age);
+//                        }
+//                        age_txtView.setText(facestringBuilder);
+//
+//
+//                        // description
+//                        for(Caption caption:result.description.captions){
+//                            stringBuilder.append(caption.text);
+//                        }
+//                        // Set our description here
+//                        desc_txtView.setText(stringBuilder);
+
+                    }
+
+                    @Override
+                    protected void onProgressUpdate(String... values) {
+                        prgDialog.setMessage(values[0]);
+                    }
+                };
+
+                // Now we just execute it
+                Log.d(TAG, "buttonClickMicrosoft: Analysis complete!");
+                visionTask.execute(inputStream);
+
             }
         });
 
